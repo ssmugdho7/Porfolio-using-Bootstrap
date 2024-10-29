b6ba08ac794acaff1f7adc07dac73036054836d7
ssmugdho7 <145373831+ssmugdho7@users.noreply.github.com>
2024-10-29 14:35:25 +0600

    Add files via upload
    Done

---
+++ Portfolio Bootstrap/js/scripts.js
@@ -0,0 +1,9 @@
+// Leaflet Map
+
+const map = L.map('map').setView([23.8195442, 90.4543555], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution:
+    '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+}).addTo(map);
